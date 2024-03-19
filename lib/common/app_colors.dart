874d6690691d57f8f2e1e6c9481ebcdda363d7d9
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-03-19 14:22:22 +0900

    feat: add color

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xffffffff);
+  static const Color orange = Color(0xffff9549);
+  static const Color black = Color(0xff262729);
+  static Color overlay = const Color(0xff262729).withOpacity(0.3);
+  static const blue1 = Color(0xffADD3FF);
+  static const Color blue2 = Color(0xffC8E1FE);
+  static const Color gray100 = Color(0xffF5F5F5);
+  static const Color gray300 = Color(0xffB8B8B8);
+}
