94048b078aec1f83f24b2b31a3b820875bab4d92
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-03-19 14:34:30 +0900

    feat: add icons

---
+++ lib/common/app_icons.dart
@@ -0,0 +1,14 @@
+class AppIcons {
+  //전역사용 아이콘
+  static const back = 'assets/icon/Icon_Back.svg';
+  static const calendar = 'assets/icon/Icon_Calendar.svg';
+  static const close = 'assets/icon/Icon_Close.svg';
+  static const deco = 'assets/icon/Icon_Deco.svg';
+  static const go = 'assets/icon/Icon_Go.svg';
+  static const locate = 'assets/icon/Icon_Locate.svg';
+  static const lock = 'assets/icon/Icon_Lock.svg';
+  static const menu = 'assets/icon/Icon_Menu.svg';
+  static const plus = 'assets/icon/Icon_Plus.svg';
+  static const search = 'assets/icon/Icon_Search.svg';
+  static const warning = 'assets/icon/Icon_Warning.svg';
+}
