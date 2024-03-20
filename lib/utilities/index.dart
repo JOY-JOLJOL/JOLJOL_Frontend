57a13620b56fbfefbfb35edcd7c8fcb6c95ae288
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-03-20 17:45:22 +0900

    Feat: Single import를 위한 index.dart 파일 추가

---
+++ lib/utilities/index.dart
@@ -0,0 +1,13 @@
+//package
+export 'package:flutter/material.dart';
+export 'package:flutter_svg/svg.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+
+//common
+export '../common/app_colors.dart';
+export '../common/app_fonts.dart';
+export '../common/app_icons.dart';
+
+//widget
+export '../presentation/widget/w_empty_box.dart';
+export '../presentation/widget/w_back_button.dart';
